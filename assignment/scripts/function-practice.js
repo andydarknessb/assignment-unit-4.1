988003e1591546a1149c1f33fb290a54c6fd9730
Cory Anderson <cory.anderson6@yahoo.com>
2023-01-31 12:15:41 -0600

    question 2

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,11 +14,12 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName( Cory ) {
+  return 'Hello, Cory!';
 }
 // Remember to call the function to test
 
+console.log(helloName("Cory"));
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber ) {
